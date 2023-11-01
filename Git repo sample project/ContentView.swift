8c4ce4d073ba64af49f81cf78eb78c6c46066743
Kloee Castaneda <kloeepratt@gmail.com>
2023-11-01 10:38:35 -0500

    Initial Commit

---
+++ Git repo sample project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git repo sample project
+//
+//  Created by Kloee Pratt-Castaneda on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git repo sample project/Git_repo_sample_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_repo_sample_projectApp.swift
+//  Git repo sample project
+//
+//  Created by Kloee Pratt-Castaneda on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_repo_sample_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
